460a96821522bdc544a3a03cf074c7582e229d9e
Nico Esposito <nico.esposito@gmail.com>
2019-03-19 11:54:17 -0300

    static folder added

--- btre/settings.py
+++ btre/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # con el comando collectStatic, levanta todos los statics roots de cada app
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # con el comando collectStatic, levanta todos los statics de cada app y lo consolida en este folder static. los consolida para el deploy
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
+    os.path.join(BASE_DIR, 'btre/static')  # se indica la carpeta donde están los statics que creamos.
 ]
