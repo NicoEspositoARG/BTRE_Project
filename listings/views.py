89ca466cd92de1144465a3ef2a28b3444224f268
Nico Esposito <nico.esposito@gmail.com>
2019-03-20 16:13:48 -0300

    pagination done

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from .models import Listing
 
+
 def index(request):
-    listings = Listing.objects.all() # 
-    
+    """Obtengo el listado de la bd, y armo el paginator para la paginación"""
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    # en lugar de usar .all, ordena por esa property en forma descendente con el -
+    # el .filter es para filtrar por alguna condicion
+
+    paginator = Paginator(listings, 6) # límite de objs x página
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     
     return render(request, 'listings/listings.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from listings.models import Listing
+
 
 def index(request):
-    # return HttpResponse("<h1>Hello World</h1>")
+    listings = Listing.order_by('-list_date').filter(is_published=True)[3]
     return render(request, 'pages/index.html')
-    
+    # return HttpResponse("<h1>Hello World</h1>")
 
 def about(request):
     return render(request, 'pages/about.html')
