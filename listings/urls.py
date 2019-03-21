5866c4531db6762a3f7945a22b52c0b3b1ef3a90
Nico Esposito <nico.esposito@gmail.com>
2019-03-21 14:37:02 -0300

    search done - class48

--- listings/urls.py
+++ listings/urls.py
@@ -5,8 +5,8 @@ from . import views
 # se vincula desde el btre urls.py para que cualquier /listings se consulte en este archivo
 
 urlpatterns = [
-    path('', views.index, name = 'listings'),  # si está vacio '' se refiere a /listings
-    path('<int:listing_id>', views.listing, name = 'listing'),  # /listings/35
-    path('search', views.search, name = 'search'),  # listings/search 
+    path('', views.index, name='listings'),  # si está vacio '' se refiere a /listings
+    path('<int:listing_id>', views.listing, name='listing'),  # /listings/35
+    path('search', views.search, name='search'),  # listings/search 
 
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -13,14 +14,14 @@ def index(request):
     # el .filter es para filtrar por alguna condicion
     # [:3] seguido al parentesis me limita a solo 3 resultados.
 
-    paginator = Paginator(listings, 6) # límite de objs x página
+    paginator = Paginator(listings, 6)  # límite de objs x página
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
     context = {
         'listings': paged_listings
     }
-    
+
     return render(request, 'listings/listings.html', context)
 
 
@@ -34,8 +35,48 @@ def listing(request, listing_id):
     }
     return render(request, 'listings/listing.html', context)
 
+
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
 
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords'] # IMPORTANTE #
+        # el GET [] busca el name del atribute html, si no definimos el name no lo va a traer
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords) # búsqueda en contenido
 
-    # return render(request, 'listings/listings.html')
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city) # búsqueda exacta, case insensitive
+
+    # City
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms) # up to entry, less than or equal to
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price) # up to entry, less than or equal to
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET # esto es para mantener los valores de búsqueda
+    }
+
+    return render(request, 'listings/search.html', context)
+
